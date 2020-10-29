d5680da3793f9813f3c2525990e2fee6bf16b02d
Claire DeBoer <claire.deboer@gmail.com>
2020-10-29 19:20:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,8 +5,9 @@ def find_even_values(src)
 row_index = 0
 while row_index < src.count do
  element_index = 0
- while element_index < spice_rack[row_index].count do
-   p spice_rack[row_index][element_index]
+ while element_index < src[row_index].count do
+   if (src[row_index][element_index])
+   p src[row_index][element_index]
    element_index += 1
  end
  row_index += 1
